ecf81f5aee2d8c3b272ade4e19c89b6b5612643d
Gruber450 <68507886+qrhrqiohj@users.noreply.github.com>
2025-04-10 22:05:26 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,43 +1,29 @@
-from concurrent.futures import ThreadPoolExecutor
-from scanner import identify_content
-from PySide6.QtWidgets import QApplication, QTableWidget, QTableWidgetItem, QVBoxLayout, QWidget
-import os
-import sys
 import time
+from concurrent.futures import ProcessPoolExecutor
+from pathlib import Path
+
+from scanner import identify_content
 
 
-# ThreadPool scanner 0.6s/10k
-path = os.path.expandvars(r"%temp%\Roblox\http")
-files = [os.path.join(path, f) for f in os.listdir(path)]
-
-def identify(file_path):
-    return os.path.basename(file_path), identify_content(file_path)
-
-start = time.time()
-with ThreadPoolExecutor(max_workers=64) as executor:
-    results = list(executor.map(identify, files))
-
-print(f"Time: {time.time() - start:.2f}s")
+ROBLOX_CACHE_PATH = Path('~/AppData/LOCAL/Temp/Roblox/http').expanduser()
 
 
-# Pyside6 Table thing
-app = QApplication(sys.argv)
-window = QWidget()
-window.setWindowTitle("Scan Results")
+def mp_identify() -> list[str]:
+    with ProcessPoolExecutor() as executor:
+        results = list(
+            executor.map(identify_content, ROBLOX_CACHE_PATH.iterdir(), chunksize=500)
+        )
+    return results
 
-table = QTableWidget()
-table.setRowCount(len(results))
-table.setColumnCount(2)
-table.setHorizontalHeaderLabels(["File Name", "Detected Type"])
 
-for row, (filename, file_type) in enumerate(results):
-    table.setItem(row, 0, QTableWidgetItem(filename))
-    table.setItem(row, 1, QTableWidgetItem(file_type))
+def main() -> None:
+    start = time.perf_counter()
+    a = mp_identify()
+    end = time.perf_counter()
 
-layout = QVBoxLayout()
-layout.addWidget(table)
-window.setLayout(layout)
-window.resize(600, 400)
-window.show()
+    print(f'Done in {end - start:.2f}s.')
+    print('Files:', len(a))
 
-sys.exit(app.exec())
+
+if __name__ == '__main__':
+    main()
